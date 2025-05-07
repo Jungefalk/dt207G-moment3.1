8ec84c0e4dfd9118eb52e0a853456e44e88c0060
Caroline <c.jungefalk@gmail.com>
2025-05-07 20:47:51 +0200

    starta server och lyssna på port

--- server.js
+++ server.js
@@ -16,3 +16,8 @@ const port = process.env.PORT || 3000;
 //Middleware
 app.use(cors());
 app.use(express.json());
+
+//Starta server
+app.listen(port,()=>{
+    console.log("Server running on port " + port)
+});
