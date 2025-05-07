d3e1134e986230cdd6b1df0affcbf53f0ff3bfb9
Caroline <c.jungefalk@gmail.com>
2025-05-07 21:04:59 +0200

    Skapa skal till routes för CRUD - operationer

--- server.js
+++ server.js
@@ -17,7 +17,34 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+//Routes
+
+//get
+app.get("/work_experience", (req, res) => {
+    res.json({ message: "Hämta alla poster" })
+});
+
+//get - med id
+app.get("/work_experience/:id", (req, res) => {
+    res.json({ message: "Hämta data baserat på id" })
+});
+
+//post
+app.post("/work_experience", (req, res) => {
+res.json({message: "Lägg till post"})
+});
+
+//put
+app.put("/work_experience/:id", (req, res) =>{
+    res.json({message: "Uppdatera post baserat på id"})
+});
+
+//delete
+app.delete("/work_experience/:id", (req, res)=>{
+    res.json({message: "Ta bort post baserat på Id"})
+})
+
 //Starta server
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("Server running on port " + port)
 });
